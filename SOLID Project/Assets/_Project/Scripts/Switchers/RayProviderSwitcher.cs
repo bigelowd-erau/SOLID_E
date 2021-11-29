deb00f30b0b594c7e7996dd3698f547c25e171a7
bigelowd-erau <88133216+bigelowd-erau@users.noreply.github.com>
2021-11-29 13:47:46 -0700

    Update RayProviderSwitcher.cs

--- SOLID Project/Assets/_Project/Scripts/Switchers/RayProviderSwitcher.cs
+++ SOLID Project/Assets/_Project/Scripts/Switchers/RayProviderSwitcher.cs
@@ -6,6 +6,6 @@ public class RayProviderSwitcher : Switcher
     private void Start()
     {
         nextKeyCode = KeyCode.Alpha2;
-        changeableObject = GetComponent<IChangeable>();//(IChangeable)GameObject.FindGameObjectWithTag("SelectionManager").GetComponent<ISelectionResponse>();
+        changeableObject = GetComponent<IChangeable>();
     }
 }
